548dd32bc1da46acb133287fdb4896d4e8cc6700
Yu Jiang Tham <yoojbruin@gmail.com>
2025-01-29 15:17:11 -0500

    Update example with more logging

--- example/src/index.ts
+++ example/src/index.ts
@@ -1,10 +1,13 @@
 import { generateRandomHex } from "@axiom-crypto/keystore-sdk/src/utils/random";
-import { AXIOM_ACCOUNT, AXIOM_ACCOUNT_AUTH_INPUTS, AXIOM_VKEY, AuthenticationStatusEnum, BlockTag, KeystoreAccountBuilder, KeystoreNodeProvider, KeystoreSequencerProvider, KeystoreSignatureProverProvider, SAMPLE_USER_CODE_HASH, SponsorAuthInputs, UpdateTransactionBuilder, UpdateTransactionRequest, calcDataHash, ecdsaSign } from "@axiom-crypto/keystore-sdk/src";
+import { AXIOM_ACCOUNT, AXIOM_ACCOUNT_AUTH_INPUTS, AXIOM_VKEY, AuthenticationStatusEnum, BlockTag, KeystoreAccountBuilder, KeystoreNodeProvider, KeystoreSequencerProvider, KeystoreSignatureProverProvider, SAMPLE_USER_CODE_HASH, SponsorAuthInputs, TransactionStatus, UpdateTransactionBuilder, UpdateTransactionRequest, calcDataHash, ecdsaSign } from "@axiom-crypto/keystore-sdk/src";
 import { Hex, hexToBigInt, stringToHex } from "viem";
 
 export const NODE_URL = "http://keystore-node-271cd8fbf8aac2f5.elb.us-east-1.amazonaws.com:80";
 export const SIGNATURE_PROVER_URL = "http://signature-prover-cee9f99ccd16c4ef.elb.us-east-1.amazonaws.com:80";
 export const SEQUENCER_URL = "http://keystore-sequencer-524685ad227fca63.elb.us-east-1.amazonaws.com:80";
+
+const RETRY_INTERVAL_SEC = 20;
+const MAX_RETRIES = 10;
 
 async function main() {
   // anvil keys
@@ -14,7 +17,7 @@ async function main() {
   const salt = generateRandomHex(32);
   const dataHash = calcDataHash(SAMPLE_USER_CODE_HASH, 1n, [eoaAddr]);
   const userAcct = KeystoreAccountBuilder.withSalt(salt, dataHash, AXIOM_VKEY);
-  console.log("user account", userAcct);
+  console.log("User account:", userAcct);
 
   const nodeProvider = new KeystoreNodeProvider(NODE_URL);
   const nonce = await nodeProvider.getTransactionCount(userAcct.keystoreAddress, BlockTag.Latest);
@@ -30,7 +33,7 @@ async function main() {
     userAcct,
     sponsorAcct: AXIOM_ACCOUNT,
   };
-  console.log("transaction request", txReq);
+  console.log("Transaction request:", txReq);
   const updateTx = UpdateTransactionBuilder.fromTransactionRequest(txReq);
   const userSig: Hex = await ecdsaSign(privateKey, updateTx.userMsgHash());
 
@@ -43,20 +46,21 @@ async function main() {
     }
   };
 
-  console.log("sending sponsor authentication request to signature prover");
+  console.log("Sending sponsor authentication request to signature prover");
 
   const signatureProverProvider = new KeystoreSignatureProverProvider(SIGNATURE_PROVER_URL);
   const requestHash = await signatureProverProvider.sponsorAuthenticateTransaction(updateTx.txBytes(), sponsorAuthInputs);
 
-  console.log("waiting for sponsor authentication to complete", requestHash);
+  console.log("Request hash:", requestHash);
+  console.log("Waiting for sponsor authentication to complete. This may take several minutes...");
 
   const authenticatedTx = await (async () => {
     while (true) {
       const status = await signatureProverProvider.getSponsorAuthenticationStatus(requestHash);
-      console.log("sponsor authentication status", status.status);
+      console.log("Sponsor authentication status:", status.status);
       switch (status.status) {
         case AuthenticationStatusEnum.Pending:
-          await new Promise(resolve => setTimeout(resolve, 20000)); // retry in 20 seconds
+          await new Promise(resolve => setTimeout(resolve, RETRY_INTERVAL_SEC * 1000));
           continue;
         case AuthenticationStatusEnum.Failed:
           throw new Error("Transaction authentication failed");
@@ -64,7 +68,7 @@ async function main() {
           if (!status.authenticatedTransaction) {
             throw new Error("No authenticated transaction found");
           }
-          console.log("sponsor authentication completed");
+          console.log("Sponsor authentication completed");
           return status.authenticatedTransaction;
         default:
           throw new Error("Invalid authentication status");
@@ -72,19 +76,29 @@ async function main() {
     }
   })();
 
-  console.log("sending transaction to sequencer");
+  console.log("Sending transaction to sequencer");
   const txHash = await sequencerProvider.sendRawTransaction(authenticatedTx);
-  console.log("transaction sent to sequencer", txHash);
+  console.log("Transaction sent to sequencer", txHash);
 
-  for (let i = 0; i < 10; i++) {
+  let currentStatus = "";
+  for (let i = 0; i < MAX_RETRIES; i++) {
     try {
       const receipt = await nodeProvider.getTransactionReceipt(txHash);
-      console.log("transaction receipt", receipt);
+      if (receipt.status !== currentStatus) {
+        currentStatus = receipt.status;
+        console.log("Transaction receipt:", receipt);
+      }
+      if (currentStatus === TransactionStatus.L2FinalizedL1Included) {
+        console.log("Success: transaction finalized!");
+        return;
+      }
+      console.log(`Checking transaction status again in ${RETRY_INTERVAL_SEC} seconds`);
     } catch (err) {
-      console.log("transaction not yet included in block");
+      console.log("Transaction not yet included in block");
     }
-    await new Promise(resolve => setTimeout(resolve, 20000)); // retry in 20 seconds
+    await new Promise(resolve => setTimeout(resolve, RETRY_INTERVAL_SEC * 1000));
   }
+  console.log(`Transaction not finalized in ${MAX_RETRIES * RETRY_INTERVAL_SEC} seconds`);
 }
 
 main()
