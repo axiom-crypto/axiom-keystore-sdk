3cc292ea7cebd94e5158a869895c0068aba52b93
Shayan Hosseini <shayan@intrinsictech.xyz>
2025-05-05 18:27:32 -0700

    chore: update bridge address

--- src/constants.ts
+++ src/constants.ts
@@ -3,4 +3,4 @@ export const KEYSTORE_CHAIN_ID = 999999999n;
 export const NODE_URL = "https://keystore-rpc-node.axiom.xyz";
 export const SEQUENCER_URL = "https://keystore-rpc-sequencer.axiom.xyz";
 
-export const BRIDGE_ADDRESS = "0x408D29C30A4A3e77aF46Cd69013e55B62BB89c62";
+export const BRIDGE_ADDRESS = "0x6C8364763d7Be106a9a9F86d9cC8990A2222ae38";
