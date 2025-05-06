6ac31ae6763a7b43fefe5ed708d1f73fe8880002
Shayan Hosseini <shayan@intrinsictech.xyz>
2025-05-06 11:03:52 -0700

    chore: update tests

--- src/constants.ts
+++ src/constants.ts
@@ -3,4 +3,4 @@ export const KEYSTORE_CHAIN_ID = 999999999n;
 export const NODE_URL = "https://keystore-rpc-node.axiom.xyz";
 export const SEQUENCER_URL = "https://keystore-rpc-sequencer.axiom.xyz";
 
-export const BRIDGE_ADDRESS = "0x6C8364763d7Be106a9a9F86d9cC8990A2222ae38";
+export const BRIDGE_ADDRESS = "0xd7304aA4F048B07f75347b84746211C196Fc2bEa";
