07e75f279dddc495af3a1808d131d163207beea5
Shayan Hosseini <shayan@intrinsictech.xyz>
2025-06-04 17:27:47 -0700

    feat: v0.2.4 release (#45)

--- src/constants.ts
+++ src/constants.ts
@@ -3,4 +3,4 @@ export const KEYSTORE_CHAIN_ID = 999999999n;
 export const NODE_URL = "https://keystore-rpc-node.axiom.xyz";
 export const SEQUENCER_URL = "https://keystore-rpc-sequencer.axiom.xyz";
 
-export const BRIDGE_ADDRESS = "0xd7304aA4F048B07f75347b84746211C196Fc2bEa";
+export const BRIDGE_ADDRESS = "0x744577A26eA68BDaA6aa8048b4387E3Fa8407dF9";
