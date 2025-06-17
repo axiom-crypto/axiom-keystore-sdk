83c100e04e4fc2a261b0e4da54e7250c227e5d98
PangZhi <1611805+PangZhi@users.noreply.github.com>
2025-06-17 13:02:15 -0700

    feat: 0.2.5 release (#50)

--- src/constants.ts
+++ src/constants.ts
@@ -3,4 +3,4 @@ export const KEYSTORE_CHAIN_ID = 999999999n;
 export const NODE_URL = "https://keystore-rpc-node.axiom.xyz";
 export const SEQUENCER_URL = "https://keystore-rpc-sequencer.axiom.xyz";
 
-export const BRIDGE_ADDRESS = "0x744577A26eA68BDaA6aa8048b4387E3Fa8407dF9";
+export const BRIDGE_ADDRESS = "0x5c948140C9A79Ea9C7CD31a899b2689e43CCb3b4";
